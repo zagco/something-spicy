9dea854ac93295ee83f3f782d816c555aa773f6d
MinenhleSolson <minenhlecele34@gmail.com>
2025-03-23 22:15:28 +0200

    14 commit

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYK7_g4BKV2VLSFmYbBCYjQEVUopGxO8s",
-  authDomain: "something-spicy-testing.firebaseapp.com",
-  projectId: "something-spicy-testing",
-  storageBucket: "something-spicy-testing.firebasestorage.app",
-  messagingSenderId: "187043341351",
-  appId: "1:187043341351:web:8ecedb1a71b40acb96da02"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 let app: FirebaseApp;
